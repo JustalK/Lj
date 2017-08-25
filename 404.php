625244c3826b7ecc8956bb694a044cfdb3a9dba9
Latsuj <justal.kevin@gmail.com>
2017-08-25 22:17:55 +0200

    Adding the 404 and starting the three.js layer

---
+++ 404.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr" class="no-js">
+
+<head>
+
+<style>
+	html,body {
+		margin: 0;
+		height: 100vh;
+		overflow: hidden;
+	}
+</style>
+
+</head>
+<body onload="start()">
+  <canvas id="glcanvas"> 
+    Votre navigateur ne semble pas supporter l'élément HTML5 <code>&lt;canvas&gt;</code>.
+  </canvas> 
+</body>
+
+<script>
+
+</html>
