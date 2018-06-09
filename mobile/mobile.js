46ee116ed491380ee0016a6ca9f7c7f2feaa67e2
Latsuj <justal.kevin@gmail.com>
2018-06-09 16:08:43 +0200

    Adding the second screen with the slide animation

--- mobile/mobile.js
+++ mobile/mobile.js
@@ -3,6 +3,8 @@ document.addEventListener("DOMContentLoaded", function() {
 	const $i = id => document.getElementById(id);
 	const $n = cn => document.getElementsByClassName(cn);
 	var bgswitch = $n("bgswitch");
+	var second = $i("SECOND");
+	var close = $n("close");
 	var tmp,ek,srck,vk;
 	
 	for(var i=bgswitch.length;i--;) {
@@ -13,7 +15,14 @@ document.addEventListener("DOMContentLoaded", function() {
 			tmp.addEventListener('load',function() {
 				this.style.backgroundImage = "url("+this.dataset.src+")";
 				this.classList.add("active");
+				second.classList.add("active");
 			}.bind(ek));
+		});
+	}
+	
+	for(var i=close.length;i--;) {
+		close[i].addEventListener('touchstart', function(e) {
+			second.classList.remove("active");
 		});
 	}
 	
